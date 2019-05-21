fixed bug in rotation

tau/data_wrangle.py
@@ -22,8 +22,8 @@ def celestial_rot_matrix(ra_point, dec_point, is_rad=True):
 
     # ADOPTED FROM: https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
     # Rotation Matrix to rotate the points vectors
-    Q = np.eye(3) - np.sin(dec_point) * K +\
-        (1 - np.cos(dec_point)) * np.matmul(K, K)
+    Q = np.eye(3) - np.cos(dec_point) * K +\
+        (1 - np.sin(dec_point)) * np.matmul(K, K)
 
     return Q
 

tau/plotting.py
@@ -39,7 +39,7 @@ def tau_scatter(x, y, z, v, limits=None):
 
 
 def visualizeMap(map_file, cfg_file, cuts, dir=2, data_file=None, width=2,
-                 prefix='test', plot_skewer=True, plot_bin=False, limits=None):
+                 prefix='test', plot_skewer=True, limits=None):
     """
     Plot the mapped data
 
@@ -57,7 +57,10 @@ def visualizeMap(map_file, cfg_file, cuts, dir=2, data_file=None, width=2,
 
     # read data file
     if data_file is not None:
-        np_data = np.loadtxt(data_file)
+        if data_file[-3:] == 'npy':
+            np_data = np.load(data_file)
+        else:
+            np_data = np.loadtxt(data_file)
         xx, yy, zz, _, vals, sk_idx = np_data.T
 
     # read map data
@@ -113,7 +116,7 @@ def visualizeMap(map_file, cfg_file, cuts, dir=2, data_file=None, width=2,
         plot_data = np.take(map_data, ele, axis=dir)
         cbar = ax[ii].imshow(plot_data.T, origin="lower", vmin=limits[0],
                              vmax=limits[1], extent=(0, l0, 0, l1),
-                             cmap=plt.cm.jet)
+                             cmap=plt.cm.seismic)
 
         # plot the raw data points
         if data_file is not None:
@@ -130,20 +133,7 @@ def visualizeMap(map_file, cfg_file, cuts, dir=2, data_file=None, width=2,
                 # overplot the scatter on the mapped plot
                 ax[ii].scatter(agg_df['x'], agg_df['y'], c=agg_df['v'],
                                vmin=limits[0], vmax=limits[1], edgecolor='k',
-                               cmap=plt.cm.jet)
-
-            if plot_bin:
-                # average over data points in a grid same as Weiner map
-                # df = binned_statistic_2d(xx[ixs], yy[ixs], vals[ixs],
-                #                          bins=[xedges, yedges]).statistic
-
-                # cbar = ax[ii].imshow(df.T, origin="lower", vmin=vmin, vmax=vmax,
-                #                      extent=(0, lx, 0, ly), cmap=plt.cm.jet)
-
-                # ax[ii].scatter(np.ravel(map_data[:, :, ele].T), np.ravel(df.T),
-                #                alpha=0.1, c='b')
-                # ax[ii].plot([-2, 2], [-2, 2])
-                pass
+                               cmap=plt.cm.seismic)
 
         ax[ii].set_title(r"$z = %.1f [h^{-1}\ Mpc]$" % centers[ele])
 

tau/tau_class.py
@@ -10,7 +10,7 @@ from astropy.io import fits
 
 
 class TauClass:
-    def __init__(self, filepath=None, spacing=4., preprocess=True, in_rad=True):
+    def __init__(self, filepath=None, spacing=4., preprocess=True):
         if filepath is None:
             filepath = sys.argv[1]
         self.data_file = fits.open(filepath)
@@ -21,7 +21,6 @@ class TauClass:
         self.pixel_data = None
 
         self.spacing = spacing
-        self.in_rad = in_rad
 
         # get locations on the celestial sphere
         if preprocess:
@@ -55,7 +54,7 @@ class TauClass:
             print("+++ Using calculated RA and DEC mean +++")
             center = [self.q_loc[:, 0].mean(), self.q_loc[:, 1].mean()]
 
-        self.rot_matrix = celestial_rot_matrix(*center, is_rad=self.in_rad)
+        self.rot_matrix = celestial_rot_matrix(*center, is_rad=False)
 
     def get_data(self, skewers_num=None, skewers_perc=1., los_sampling=3,
                  xtype='loglam', plotit=False, **kwargs):
@@ -94,13 +93,10 @@ class TauClass:
         print('****** READING ****** \n')
         for i in ixs:
             tb = self.data_file[i].data
-            rcomov = tb['RCOMOV']
+            rcomov = tb['RCOMOV'][::los_sampling]
 
             # convert spherical to cartesian cordinates
-            ra, dec = self.q_loc[i-1, :2]
-
-            if not self.in_rad:
-                ra, dec = np.deg2rad([ra, dec])
+            ra, dec = np.deg2rad(self.q_loc[i-1, :2])
 
             x = np.cos(dec) * np.cos(ra) * rcomov
             y = np.cos(dec) * np.sin(ra) * rcomov